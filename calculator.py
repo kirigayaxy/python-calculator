569c0bf3b51e5341a8376876ac56928cc21c836f
kirigayaxy <thesingerofopera@gmail.com>
2024-11-16 15:41:45 +0000

    Added the test cases

--- calculator.py
+++ calculator.py
@@ -3,25 +3,68 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
+        if b >=0:
+            for i in range(b):
+                result = self.add(result, a)
+        else:
+            for i in range(a):
+                result = self.add(result, b)
         return result
 
     def divide(self, a, b):
+        print(a)
+        print(b)
         result = 0
-        while a > b:
-            a = self.subtract(a, b)
-            result += 1
+        if a > 0 and b >0:
+            while a >= b:
+
+                a = self.subtract(a, b)
+
+                result += 1
+        elif a>0 and b==0:
+            return ("cannot divide")
+                    
+        elif a >=0 and b<0:
+            while a >= b:
+                if a ==0:
+                    break
+                else:
+
+                    a = self.add(a, b)
+
+                    result -= 1
+        elif a<0 and b>=0:
+            while b >= a:
+                if a ==0:
+                    break
+                if b ==0:
+                    return ("cannot divide")
+                else:
+
+                    a = self.add(a, b)
+
+                    result -= 1
+        elif a<0 and b<0 and a>b :
+            return result
+        elif a<0 and b<0 and a<b :
+            while b >= a:
+
+                a = self.subtract(a, b)
+
+                result += 1
+        else:
+            return result
         return result
     
     def modulo(self, a, b):
-        while a <= b:
+        while a >= b:
             a = a-b
         return a
+
 
 # Example usage:
 if __name__ == "__main__":
--- test_calculator.py
+++ test_calculator.py
@@ -8,6 +8,24 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(-2, 4), 2)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(4, 2), 2)
+        self.assertEqual(self.calc.subtract(-1, -4), 3)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 0), 0)
+        self.assertEqual(self.calc.multiply(2, -4), -8)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(8, -4), -2)
+        self.assertEqual(self.calc.divide(-6, 3), -2)
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(7, 2), 1)
+        self.assertEqual(self.calc.modulo(3, 5), 3)
+
 
     # Add the following test methods to the TestCalculator class:
 
